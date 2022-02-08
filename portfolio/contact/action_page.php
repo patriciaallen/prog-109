0141582223eae97072a988e1ea4ba7907640aae4
Patricia Allen <97283176+patriciaallen@users.noreply.github.com>
2022-02-07 17:11:45 -0800

    initial push

---
+++ portfolio/contact/action_page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+         <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/liera-sans" type="text/css"/>
+        <link rel="stylesheet" media="screen" href="https://patriciaallen.github.io/prog-109/portfolio/style.css" type="text/css"/>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+    <title>Contact Me</title>
+   
+<body>
+
+    <h1>Contact Me</h1>
+
+
+<h3>Navigation</h3>
+
+<div id="nav">
+<ul>
+    <li><a href="https://patriciaallen.github.io/prog-109/portfolio/homepage">Home</a></li>
+    <li><a href="https://patriciaallen.github.io/prog-109/portfolio/aboutme">About</a></li>
+    <li><a href="https://patriciaallen.github.io/prog-109/portfolio/coursework">Coursework</a></li>
+    <li><a href="https://patriciaallen.github.io/prog-109/portfolio/artifacts">Artifacts</a></li>
+    <li><a href="https://patriciaallen.github.io/prog-109/portfolio/module">Module Assignments</a></li>
+    <li><a href="https://patriciaallen.github.io/prog-109/portfolio/contact">Contact me</a></li>
+    <li><a href="https://patriciaallen.github.io/prog-109/portfolio/own">Own</a></li>
+</ul>
+</div>
+  
+  <p>Thanks for visiting my page :)</p>
+  </body>
+</html>
