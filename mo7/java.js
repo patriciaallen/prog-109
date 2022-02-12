9a8eaa1979a832741ea9471f9e9a57d1f3d247e7
Patricia Allen <97283176+patriciaallen@users.noreply.github.com>
2022-02-12 01:48:25 -0800

    Update java.js

--- mo7/java.js
+++ mo7/java.js
@@ -1,20 +1,18 @@
 // Create the template for objects that are hotels
-class Hotel {
-    constructor(name, rooms, booked) {
-        this.name = name;
-        this.rooms = rooms;
-        this.booked = booked;
-        this.checkAvailability = function () {
-            return this.rooms - this.booked;
-        };
-    }
-}
+function Hotel(name, rooms, booked) {
+    this.name = name;
+    this.rooms = rooms;
+    this.booked = booked;
+    this.checkAvailability = function() {
+      return this.rooms - this.booked;
+    };
+  }
   
   
   // Create two hotel objects
   var quayHotel = new Hotel('Quay', 40, 25);
   var parkHotel = new Hotel('Park', 120, 77);
-  var sunsetHotel = new Hotel('Sunset', 86, 10);
+  var hotel3 = new Hotel('Sunset', 86, 10);
   
   
   // Update the HTML for the page
